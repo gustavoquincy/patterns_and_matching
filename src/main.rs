b3904e0eac4f0d08a0769fd6c50144baa85189e3
gustavoquincy <yuankaile@gmail.com>
2022-05-02 22:46:12 +0800

    A function with parameters taht destructure a tuple

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,8 @@
+fn print_coordinate(&(x, y): &(i32, i32)) {
+    println!("Current location: ({}, {})", x, y);
+}
+
 fn main() {
-    let v = vec!['a', 'b', 'c'];
-    
-    for (index, value) in v.iter().enumerate() {
-        println!("{} is at index {}", value, index);
-    }
+    let point = (3, 5);
+    print_coordinate(&point);
 }
