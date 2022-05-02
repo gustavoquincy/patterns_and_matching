dc1f122f4b5459b0f6860d3676674a473075a2c8
gustavoquincy <yuankaile@gmail.com>
2022-05-02 22:39:26 +0800

    Using a `while let` loop to print values for as long as `stack.pop()` returns `Some`

--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,13 @@
 fn main() {
-    let favorite_color: Option<&str> = None;
-    let is_tuesday = false;
-    let age: Result<u8, _> = "34".parse();
+    let mut stack = Vec::new();
 
-    if let Some(color) = favorite_color {
-        println!("Using your favorite color, {}, as the background", color);
-    } else if is_tuesday {
-        println!("Tuesday is green day!");
-    } else if let Ok(age) = age {
-        if age > 30 {
-            println!("Using purple as the background color");
-        } else {
-            println!("Using orange as the background color");
-        }
-    } else {
-        println!("Using blue as the background color");
+    stack.push(1);
+    stack.push(2);
+    stack.push(3);
+
+    while let Some(top) = stack.pop() {
+        println!("{}", top);
     }
+
+    println!("{:?}", stack);
 }
