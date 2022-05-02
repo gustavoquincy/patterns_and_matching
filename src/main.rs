b19ecd7a294b3b9710c853d109c8aec103bf8ce6
gustavoquincy <yuankaile@gmail.com>
2022-05-02 22:41:49 +0800

    Using a pattern in a `for` loop to desturcture a tuple

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,7 @@
 fn main() {
-    let mut stack = Vec::new();
-
-    stack.push(1);
-    stack.push(2);
-    stack.push(3);
-
-    while let Some(top) = stack.pop() {
-        println!("{}", top);
+    let v = vec!['a', 'b', 'c'];
+    
+    for (index, value) in v.iter().enumerate() {
+        println!("{} is at index {}", value, index);
     }
-
-    println!("{:?}", stack);
 }
