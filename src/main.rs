ea4d2b899456b6a1343de9970f866a5da885b492
gustavoquincy <yuankaile@gmail.com>
2022-05-03 12:30:53 +0800

    Ignoring all fields of a `Point` except for `x` by using `..`

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,14 @@
 fn main() {
-    let numbers = (2, 4, 8, 16, 32);
-
-    match numbers {
-        (first, _, third, _, fifth) => {
-            println!("Some numbers: {}, {}, {}", first, third, fifth);
-        }
+    struct Point {
+        x: i32,
+        y: i32,
+        z: i32,
     }
+
+    let origin = Point { x: 0, y: 0, z: 0 };
+
+    match origin {
+        Point { x, .. } => println!("x is {}", x),
+    }
+
 }
