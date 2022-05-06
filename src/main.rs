6962975df42f66fab23048dd8d5c96d9b992a02a
gustavoquincy <yuankaile@gmail.com>
2022-05-06 15:47:47 +0800

    Using `@` to bind to a value in a pattern while also testing it

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,16 @@
 fn main() {
-    struct Point {
-        x: i32,
-        y: i32,
-        z: i32,
+    enum Message {
+        Hello { id: i32 },
     }
 
-    let origin = Point { x: 0, y: 0, z: 0 };
+    let msg = Message::Hello { id: 5 };
 
-    match origin {
-        Point { x, .. } => println!("x is {}", x),
+    match msg {
+        Message::Hello { id: id_variable @ 3..=7 } => 
+        println!("Found an id in range: {}", id_variable),
+        Message::Hello { id: 10..=12 } =>
+        println!("Found an id in another range"),
+        Message::Hello { id } => 
+        println!("Found some other id: {}", id),
     }
-
 }
